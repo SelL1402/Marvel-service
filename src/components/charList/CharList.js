40eb13fe65948c617767e7bbf9a09b97cc5c144b
Hudzenko Valentyn <gfd.lah@gmail.com>
2024-05-13 12:14:36 +0300

    Fixed a bug with character selection on the main page

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect, useMemo} from 'react';
 import PropTypes from 'prop-types';
 import useMarvelService from '../../services/MarvelServices';
 import Spinner from '../spinner/Spinner';
@@ -85,11 +85,13 @@ const CharList = (props) => {
             </ul>
         )
     }
-
+    const elements = useMemo(()=>{
+        return  setContent(process, () => renderItems(charlist), newItemLoading)
+    }, [process])
     
     return (
         <div className='char__list'>
-            {setContent(process, () => renderItems(charlist), newItemLoading)}
+            {elements}
             <button 
                 className='button button__main button__long'
                 disabled={newItemLoading}
